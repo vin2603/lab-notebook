4d70e306675e6c8fa0d17826e4c3eb2c4fdae65b
Vincent Le <vincentle@Vincents-MacBook-Air.local>
2025-06-09 14:09:33 -0700

    Lab 7 completed

--- 07-javascript/04-dom.js
+++ 07-javascript/04-dom.js
@@ -10,8 +10,29 @@
 // Exercise #2:
 // when the user enters input text, copy the user input to the output area
 
+let userInput = document.querySelector("#userInput1");
+let copy = document.querySelector("#copy");
+let output = document.querySelector(".output");
+
+copy.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  output.textContent = userInput.value;
+}
+
 // fetch JavaScript objects representing specific elements in the DOM
 
 // add an event listener on the target element
 
 // callback function to handle event
+let userInput2 = document.querySelector("#userInput2");
+userInput2.addEventListener("input", handleInput);
+
+let section = document.querySelector("#inputEventExample");
+let element = document.createElement("div");
+element.setAttribute("class", "output");
+section.append(element);
+function handleInput(event) {
+  console.log(userInput2.value);
+  element.textContent = userInput2.value;
+}
